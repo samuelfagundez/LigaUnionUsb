1846e32e023a656300e1ce20e34b1673bdc7c2d7
David Rod <dave00dark@gmail.com>
2019-01-16 23:34:17 -0400

    Añadido la app futsal al proyecto

--- ligaUnion/ligaUnion/settings.py
+++ ligaUnion/ligaUnion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wx7zxg-u&=ym1m$#!qis34kk(p4oux))&#wcogd*&*xu9^iln9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'futsal',
 ]
 
 MIDDLEWARE = [
