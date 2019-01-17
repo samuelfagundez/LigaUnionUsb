45ae23c85ad2127511e81f786083cae8438bd701
David Rod <dave00dark@gmail.com>
2019-01-16 23:33:25 -0400

    Pequeñas modificaciones de la base de datos y añadir un metodo str a los modelos

--- ligaUnion/futsal/models.py
+++ ligaUnion/futsal/models.py
@@ -13,25 +13,39 @@ ESTADOS = (
 	('TE', 'Terminado')
 ) 
 
-class Jugado(models.Model):
-	nombre = models.CharField(max_length=100, blank=False, Null=False)
+class Jugador(models.Model):
+	nombre = models.CharField(max_length=100, blank=False, null=False)
 	goles = models.PositiveSmallIntegerField(default=0)
 	t_amarillas = models.PositiveSmallIntegerField(default=0)
 	t_rojas = models.PositiveSmallIntegerField(default=0)
 	equipo = models.ForeignKey('Equipo', on_delete=models.CASCADE)
 
+	def __str__(self):
+		return self.nombre
+
 class Equipo(models.Model):
-	nombre = models.CharField(max_length=100, blank=False, Null=False)
+	nombre = models.CharField(max_length=100, blank=False, null=False)
 	p_ganados = models.PositiveSmallIntegerField(default=0)
 	p_empatados = models.PositiveSmallIntegerField(default=0)
 	p_perdidos = models.PositiveSmallIntegerField(default=0)
 	grupo = models.ForeignKey('Grupo', on_delete=models.CASCADE)
 
+	def __str__(self):
+		return self.nombre
+
 class Grupo(models.Model):
-	numero = models.PositiveSmallIntegerField(blank=False, Null=False)
+	numero = models.PositiveSmallIntegerField(blank=False, null=False)
+
+	def __str__(self):
+		return str(self.numero)
 
 class Partido(models.Model):
 	fecha = models.DateTimeField()
-	equipo1 = models.OneToOneField('Equipo', models.SET_NULL)
-	equipo2 = models.OneToOneField('Equipo', models.SET_NULL)
+	equipo1 = models.OneToOneField('Equipo', models.SET_NULL, related_name='Equipo1', null=True)
+	equipo2 = models.OneToOneField('Equipo', models.SET_NULL, related_name='Equipo2', null=True)
 	estado = models.CharField(max_length=2, choices=ESTADOS, default='NJ')
+	resultado1 = models.PositiveSmallIntegerField(blank=True, null=True)
+	resultado2 = models.PositiveSmallIntegerField(blank=True, null=True)
+
+	def __str__(self):
+		return self.equipo1 + ' vs ' + self.equipo2
