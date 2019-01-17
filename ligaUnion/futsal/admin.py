dfc2712768c1497061222939574c45f3c47cbbed
David Rod <dave00dark@gmail.com>
2019-01-16 23:32:51 -0400

    Añadido los modelos a las vistas de admin

--- ligaUnion/futsal/admin.py
+++ ligaUnion/futsal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Jugador, Equipo, Grupo, Partido
 
 # Register your models here.
+
+admin.site.register(Jugador)
+admin.site.register(Equipo)
+admin.site.register(Grupo)
+admin.site.register(Partido)
